273e596fb5ff6dddb0fc8f103b852ca488543cdd
Andrej Kyselica <andrej@kyselica.com>
2017-02-24 17:01:42 -0500

    separated client and overwatch client JS libraries

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ var BitBarWebpackProgressPlugin = require("bitbar-webpack-progress-plugin");
 var path = require('path');
 
 module.exports = {
-  entry: './client/index-client.js',
+  entry: {
+      client: './client/index-client.js',
+      overwatch: './client/index-overwatch.js'
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'website')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'website'),
+    chunkFilename: "[id].chunk.js"
   },
   plugins: [
     new BitBarWebpackProgressPlugin()
